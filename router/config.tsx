23efcd2dbc3609bd9c6db8a2f4f7a6dc7775244f
Lakshay Panchal <lakshaypanchal89.com>
2025-11-04 15:29:52 +0530

    asb

--- router/config.tsx
+++ router/config.tsx
@@ -1,5 +1,5 @@
-
-import { RouteObject } from 'react-router-dom';
+// Use type import to avoid runtime import issues with RouteObject
+import type { RouteObject } from 'react-router-dom';
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import('../pages/home/page'));
